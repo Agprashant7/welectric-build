028a2f3e891281ab95b69ce5750c652d0d018685
Prashant Ganiger <prashant.ganiger@unistory.co>
2020-09-25 14:39:31 +0530

    inital commit

---
+++ precache-manifest.b085172301a10ca90e3d308c08302ff5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2902ca248c38ee32561be9feeb5ed26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1616e7b5f65adb383b8f",
+    "url": "/static/css/main.cbe50620.chunk.css"
+  },
+  {
+    "revision": "c5a8379cd67b4847e803",
+    "url": "/static/js/2.b4d999fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4d999fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1616e7b5f65adb383b8f",
+    "url": "/static/js/main.e6893ff7.chunk.js"
+  },
+  {
+    "revision": "43b25eb14975ab1b1919",
+    "url": "/static/js/runtime-main.146b4aeb.js"
+  },
+  {
+    "revision": "0aedf31c6bbe922cc740cfd9e634e2e0",
+    "url": "/static/media/1.0aedf31c.webp"
+  },
+  {
+    "revision": "208525dbfd66216d4c7fd17d00033c54",
+    "url": "/static/media/1.208525db.jpg"
+  },
+  {
+    "revision": "05b647e8df8689616c8458b9eb562813",
+    "url": "/static/media/2.05b647e8.jpg"
+  },
+  {
+    "revision": "ce8895f10802f6c848b49c591846de5e",
+    "url": "/static/media/3.ce8895f1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
